2eb6b84dedbfb041e843b0cc85e74a04771dda92
Geoff Pawlowski <gpawlows@gmail.com>
2021-09-06 20:10:36 -0700

    zipcodes solved

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
 api_key = "626e1a8fdf10ed323ea4e49737d634f6a3b0503a"
+remote_db_endpoint = 'bulldogs-3.cznzpremohjj.us-east-2.rds.amazonaws.com'
+remote_db_port = '5432'
+remote_db_user = 'postgres'
+remote_db_pwd = 'postgres80'
+remote_db_name = 'Bulldogs'
